6797e2170f75274000b4622ca71f48505c8dcd47
Carolina Pacheco <90946121+carolinapsil@users.noreply.github.com>
2022-02-02 23:47:24 -0300

    Revert "Criação entidade Voluntario"
    This reverts commit 045a78f4c738170d5b7028f3adaae32c76e758a2.

--- MVC/entidade/doador.py
+++
@@ -1,6 +0,0 @@
-from pessoa import Pessoa
-
-class Doador(Pessoa):
-
-    def __init__(self, nome: str, data_nascimento: str, telefone: str, genero: str, email: str, endereco: str):
-        super.__init__(nome, data_nascimento, telefone, genero, email, endereco)
