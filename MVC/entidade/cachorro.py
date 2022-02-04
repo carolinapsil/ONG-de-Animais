2c99d398c377d3bc9fc436249be197dae5a0ce1d
Aline Cristina Meyer <aline.meyer@gmail.com>
2022-02-04 16:00:46 -0300

    Criaçao de Class Gato e Cachorro

--- MVC/entidade/cachorro.py
+++ MVC/entidade/cachorro.py
@@ -4,5 +4,5 @@ from animal import Animal
 class Cachorro(Animal):
 
     def __init__(self, nome: str, chegada: str, ano_nascimento: str, sexo: str, doenca: str, vacina: str, castracao: str, porte: str):
-        self.__porte = porte
+        #self.__porte = porte
         super.__init__(nome, chegada, ano_nascimento, sexo, doenca, vacina, castracao)
