7fe50d46bfa206abf9c068039d25e284a9675abf
Carolina Pacheco <90946121+carolinapsil@users.noreply.github.com>
2022-02-03 22:38:44 -0300

    Correção de caracteres TelaDoacao

--- MVC/limite/tela_doacao.py
+++ MVC/limite/tela_doacao.py
@@ -1,12 +1,12 @@
 class TelaDoacao():
   # fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
   def tela_opcoes(self):
-    print("-------- DOAÇÃO ----------")
+    print("-------- DOACAO ----------")
     print("Escolha a opcao")
-    print("1 - Incluir Doação")
-    print("2 - Alterar Doação")
-    print("3 - Listar Doação")
-    print("4 - Excluir Doação")
+    print("1 - Incluir Doacao")
+    print("2 - Alterar Doacao")
+    print("3 - Listar Doacao")
+    print("4 - Excluir Doacao")
     print("0 - Retornar")
 
     opcao = int(input("Escolha a opcao: "))
@@ -14,24 +14,24 @@ class TelaDoacao():
 
   # fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
   def pega_dados_doacao(self):
-    print("-------- DADOS DOAÇÃO ----------")
+    print("-------- DADOS DOACAO ----------")
     doador = input("Doador: ")
     valor = input("Valor doado: ")
-    data_doacao = input("Data de doação: ")
-    codigo = input("Código da doação: ")
+    data_doacao = input("Data de doacao: ")
+    codigo = input("Codigo da doacao: ")
 
     return {"doador": doador, "valor": valor, "data_doacao": data_doacao, "codigo": codigo}
 
   # fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
   def mostra_doacao(self, dados_doacao):
     print("DOADOR: ", dados_doacao["doador"])
-    print("VALOR DA DOAÇÃO: ", dados_doacao["valor"])
-    print("DATA DA DOAÇÃO: ", dados_doacao["data_doacao"])
+    print("VALOR DA DOACAO: ", dados_doacao["valor"])
+    print("DATA DA DOACAO: ", dados_doacao["data_doacao"])
     print("\n")
 
   # fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
   def seleciona_doacao(self):
-    codigo = input("Código da doação que deseja selecionar: ")
+    codigo = input("Codigo da doacao que deseja selecionar: ")
     return codigo
 
   def mostra_mensagem(self, msg):
