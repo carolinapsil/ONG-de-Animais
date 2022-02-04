63fab77bba80f0a9674180093b1aef0c6a55190a
Aline Cristina Meyer <aline.meyer@gmail.com>
2022-02-04 15:06:11 -0300

    Criação de arquivos e conserto de código

--- MVC/entidade/voluntario.py
+++ MVC/entidade/voluntario.py
@@ -1,5 +1,6 @@
 from pessoa import Pessoa
 
+
 class Voluntario(Pessoa):
 
     def __init__(self, nome: str, data_nascimento: str, telefone: str, genero: str, email: str, endereco: str, oferece_lt: str):
