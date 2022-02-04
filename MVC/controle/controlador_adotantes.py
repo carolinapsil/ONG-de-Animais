f52ae29a798454bd57ac1195108e90806bfeffdd
Carolina Pacheco <90946121+carolinapsil@users.noreply.github.com>
2022-02-04 01:50:12 -0300

    Adição de métodos Controlador Adotantes

--- MVC/controle/controlador_adotantes.py
+++ MVC/controle/controlador_adotantes.py
@@ -7,3 +7,20 @@ class ControladorAdotantes():
         self.__tela_adotante = TelaAdotante
         self.__controlador_sistema = controlador_sistema
 
+    def inicia(self):
+        self.abre_tela_inicial()
+
+    def inclui_adotante(self):
+        pass
+
+    def altera_adotante(self):
+        pass
+
+    def exclui_adotante(self):
+        pass
+
+    def finalizar(self):
+        pass
+
+    def abre_tela_inicial(self):
+        switcher = (1: self.inclui_adotante, 2: self.altera_adotante, 3: self.lista_adotante, 4: self.exclui_adotante, 0: self.finalizar)
