55363237d2da8160ac843698643179c57e6e93e9
Carolina Pacheco <90946121+carolinapsil@users.noreply.github.com>
2022-02-04 02:36:23 -0300

    Código métodos Controlador Adotantes

--- MVC/controle/controlador_adotantes.py
+++ MVC/controle/controlador_adotantes.py
@@ -10,17 +10,55 @@ class ControladorAdotantes():
     def inicia(self):
         self.abre_tela_inicial()
 
+    def pega_adotante_por_telefone(self, telefone: str):
+        for adotante in self.__adotantes:
+            if (adotante.telefone == telefone):
+                return adotante
+        return None
+
     def inclui_adotante(self):
-        pass
+        dados_adotante = self.__tela_adotante.pega_dados_adotante()
+        adotante = Adotante(dados_adotante["nome"],dados_adotante["data_nascimento"], dados_adotante["telefone"],
+                            dados_adotante["genero"], dados_adotante["email"], dados_adotante["endereco"])
+        self.__adotantes.append(adotante)
 
     def altera_adotante(self):
-        pass
+        self.lista_adotantes()
+        telefone_adotante = self.__tela_adotante.seleciona_adotante()
+        adotante = self.pega_adotante_por_telefone(telefone_adotante)
+
+        if(adotante is not None):
+          novos_dados_adotante = self.__tela_adotante.pega_dados_adotante()
+          adotante.nome = novos_dados_adotante["nome"]
+          adotante.data_nascimento = novos_dados_adotante["data_nascimento"]
+          adotante.telefone = novos_dados_adotante["telefone"]
+          adotante.genero = novos_dados_adotante["genero"]
+          adotante.email = novos_dados_adotante["email"]
+          adotante.endereco = novos_dados_adotante["endereco"]
+          self.lista_adotantes()
+        else:
+          self.__tela_adotante.mostra_mensagem("ATENCAO: Adotante não existente")
+
+    def lista_adotantes(self):
+        for adotante in self.__adotantes:
+            self.__tela_adotante.mostra_adotante({"nome": adotante.nome, "data_nascimento": adotante.data_nascimento,
+                                                  "telefone": adotante.telefone, "genero": adotante.genero, "email":
+                                                      adotante.email, "endereco": adotante.endereco})
 
     def exclui_adotante(self):
-        pass
+        self.lista_adotantes()
+        telefone_adotante = self.__tela_adotante.seleciona_adotante()
+        adotante = self.pega_adotante_por_telefone(telefone_adotante)
 
-    def finalizar(self):
-        pass
+        if (adotante is not None):
+            self.__adotantes.remove(adotante)
+            self.lista_adotantes()
+        else:
+            self.__tela_adotante.mostra_mensagem("ATENCAO: Adotante não existente")
+
+    def retornar(self):
+        self.__controlador_sistema.abre_tela()
 
     def abre_tela_inicial(self):
-        switcher = (1: self.inclui_adotante, 2: self.altera_adotante, 3: self.lista_adotante, 4: self.exclui_adotante, 0: self.finalizar)
+        switcher = {1: self.inclui_adotante, 2: self.altera_adotante, 3: self.lista_adotante, 4: self.exclui_adotante,
+                        0: self.retornar}
