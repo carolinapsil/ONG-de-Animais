4b2cc20f8e88032a2f834d3606fbdac3c77ed70f
Carolina Pacheco <90946121+carolinapsil@users.noreply.github.com>
2022-02-04 22:44:31 -0300

    Código Controlador ONG

---
+++ MVC/controle/controlador_ong.py
@@ -0,0 +1,18 @@
+from MVC.controle.controlador_doacao import ControladorDoacoes
+from MVC.limite.tela_principal import TelaPrincipal
+
+class ControladorONG:
+
+    def __init__(self):
+        self.__controlador_doacao = ControladorDoacoes(self)
+        #colocar todos os controladores
+        self.__tela_principal = TelaPrincipal()
+
+    def inicia_sistema(self):
+        opcao = self.__tela_principal.mostra_opcoes()
+        if opcao == 1:
+            self.__controlador_doacao.mostra_opcoes()
+
+
+controlador_ong = ControladorONG()
+controlador_ong.inicia_sistema()
