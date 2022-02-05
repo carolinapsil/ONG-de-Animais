4e8a7bde7087603e2a24bfe83b1d1d6fbbe59a72
Carolina Pacheco <90946121+carolinapsil@users.noreply.github.com>
2022-02-04 22:46:18 -0300

    Ajustes controlador Doação

--- MVC/controle/controlador_doacao.py
+++ MVC/controle/controlador_doacao.py
@@ -4,7 +4,7 @@ from MVC.entidade.doacao import Doacao
 class ControladorDoacoes():
     def __init__(self, controlador_sistema):
         self.__doacoes = []
-        self.__tela_doacoes = TelaDoacao
+        self.__tela_doacao = TelaDoacao()
         self.__controlador_sistema = controlador_sistema
 
     def pega_doacao_por_codigo(self, codigo: int):
@@ -16,15 +16,34 @@ class ControladorDoacoes():
     def incluir_doacao(self):
         self.__controlador_sistema.controlador_doadores.lista_doadores()
         self.__controlador_sistema.controlador_doacoes.lista_doacao()
-        dados_doacao = self.__tela_doacoes.pega_dados_doacao()
+        dados_doacao = self.__tela_doacao.pega_dados_doacao()
+
+    def altera_doacao(self):
+        self.lista_doacao()
+        codigo_doacao = self.__tela_doacao.seleciona_doacao()
+        doacao = self.pega_doacao_por_codigo(codigo_doacao)
+
+        if(doacao is not None):
+          novos_dados_doacao = self.__doacoes.pega_dados_doacao()
+          doacao.doador = novos_dados_doacao["doador"]
+          doacao.valor = novos_dados_doacao["valor"]
+          doacao.data_doacao = novos_dados_doacao["data_doacao"]
+          doacao.codigo = novos_dados_doacao["codigo"]
+          self.lista_doacao()
+        else:
+          self.__tela_doacao.mostra_mensagem("ATENCAO: Doacao não existente")
+
 
     def excluir_doacao(self):
         self.lista_doacao()
-        codigo_doacao = self.__tela_doacoes.seleciona_doacao()
+        codigo_doacao = self.__tela_doacao.seleciona_doacao()
         doacao = self.pega_doacao_por_codigo(int(codigo_doacao))
 
         if (doacao is not None):
             self.__doacoes.remove(doacao)
             self.lista_doacao()
         else:
-            self.__tela_doacoes.mostra_mensagem("ATENCAO: Doacao não existente")
+            self.__tela_doacao.mostra_mensagem("ATENCAO: Doacao não existente")
+
+    def mostra_opcoes(self):
+        self.__tela_doacao.tela_opcoes()
