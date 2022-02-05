15bf30954f2789f3ef1908b955e4221df6776628
Carolina Pacheco <90946121+carolinapsil@users.noreply.github.com>
2022-02-05 02:16:59 -0300

    Adição opções Tela Principal

--- MVC/limite/tela_principal.py
+++ MVC/limite/tela_principal.py
@@ -6,6 +6,8 @@ class TelaPrincipal():
     def mostra_opcoes(self):
         print("1 - Doacoes")
         print("2 - Animais")
-        print("3 - Pessoas")
+        print("3 - Adotante")
+        print("4 - Doador")
+        print("5 - Voluntario")
         opcao_escolhida = int(input("Escolha opcao:"))
         return opcao_escolhida
