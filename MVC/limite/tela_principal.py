7d7a63ce24a023127ea86d99db371e2cc6d071b3
Carolina Pacheco <90946121+carolinapsil@users.noreply.github.com>
2022-02-04 22:45:08 -0300

    Código Tela Principal

---
+++ MVC/limite/tela_principal.py
@@ -0,0 +1,11 @@
+class TelaPrincipal():
+
+    def __init__(self):
+        pass
+
+    def mostra_opcoes(self):
+        print("1 - Doacoes")
+        print("2 - Animais")
+        print("3 - Pessoas")
+        opcao_escolhida = int(input("Escolha opcao:"))
+        return opcao_escolhida
