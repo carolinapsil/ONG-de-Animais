326c6e8170144336572fce297ad58e2d474f204b
Aline Cristina Meyer <aline.meyer@gmail.com>
2022-02-05 02:07:11 -0300

    conserto indentaçao TelaDoacao

--- MVC/limite/tela_doacao.py
+++ MVC/limite/tela_doacao.py
@@ -1,38 +1,34 @@
-class TelaDoacao():
-  # fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
-  def tela_opcoes(self):
-    print("-------- DOACAO ----------")
-    print("Escolha a opcao")
-    print("1 - Incluir Doacao")
-    print("2 - Alterar Doacao")
-    print("3 - Listar Doacao")
-    print("4 - Excluir Doacao")
-    print("0 - Retornar")
+class TelaDoacao:
+    def tela_opcoes(self):
+        print("-------- DOACAO ----------")
+        print("Escolha a opcao")
+        print("1 - Incluir Doacao")
+        print("2 - Alterar Doacao")
+        print("3 - Listar Doacao")
+        print("4 - Excluir Doacao")
+        print("0 - Retornar")
 
-    opcao = int(input("Escolha a opcao: "))
-    return opcao
+        opcao = int(input("Escolha a opcao: "))
+        return opcao
 
-  # fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
-  def pega_dados_doacao(self):
-    print("-------- DADOS DOACAO ----------")
-    doador = input("Doador: ")
-    valor = input("Valor doado: ")
-    data_doacao = input("Data de doacao: ")
-    codigo = input("Codigo da doacao: ")
+    def pega_dados_doacao(self):
+        print("-------- DADOS DOACAO ----------")
+        doador = input("Doador: ")
+        valor = input("Valor doado: ")
+        data_doacao = input("Data de doacao: ")
+        codigo = input("Codigo da doacao: ")
 
-    return {"doador": doador, "valor": valor, "data_doacao": data_doacao, "codigo": codigo}
+        return {"doador": doador, "valor": valor, "data_doacao": data_doacao, "codigo": codigo}
 
-  # fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
-  def mostra_doacao(self, dados_doacao):
-    print("DOADOR: ", dados_doacao["doador"])
-    print("VALOR DA DOACAO: ", dados_doacao["valor"])
-    print("DATA DA DOACAO: ", dados_doacao["data_doacao"])
-    print("\n")
+    def mostra_doacao(self, dados_doacao):
+        print("DOADOR: ", dados_doacao["doador"])
+        print("VALOR DA DOACAO: ", dados_doacao["valor"])
+        print("DATA DA DOACAO: ", dados_doacao["data_doacao"])
+        print("\n")
 
-  # fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
-  def seleciona_doacao(self):
-    codigo = input("Codigo da doacao que deseja selecionar: ")
-    return codigo
+    def seleciona_doacao(self):
+        codigo = input("Codigo da doacao que deseja selecionar: ")
+        return codigo
 
-  def mostra_mensagem(self, msg):
-    print(msg)
+    def mostra_mensagem(self, msg):
+        print(msg)
