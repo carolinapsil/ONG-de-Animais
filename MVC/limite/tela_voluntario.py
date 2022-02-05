bcb0439dc83c150b97f43d7033b310e1871e38ff
Carolina Pacheco <90946121+carolinapsil@users.noreply.github.com>
2022-02-05 02:48:15 -0300

    Criação e código Tela Voluntario

---
+++ MVC/limite/tela_voluntario.py
@@ -0,0 +1,42 @@
+class TelaVoluntario:
+    def tela_opcoes(self):
+        print("-------- VOLUNTARIO ----------")
+        print("Escolha a opcao")
+        print("1 - Incluir Voluntario")
+        print("2 - Alterar Voluntario")
+        print("3 - Listar Voluntario")
+        print("4 - Excluir Voluntario")
+        print("0 - Retornar")
+
+        opcao = int(input("Escolha a opcao: "))
+        return opcao
+
+    def pega_dados_voluntario(self):
+        print("-------- DADOS VOLUNTARIO ----------")
+        nome = input("Nome: ")
+        data_nascimento = input("Data de nascimento: ")
+        telefone = input("Telefone: ")
+        genero = input("Genero: ")
+        email = input("E-mail: ")
+        endereco = input("Endereco: ")
+        oferece_lt = input("Oferece lar temporario: ")
+
+        return {"nome": nome, "data nascimento": data_nascimento, "telefone": telefone, "genero": genero,
+                "email": email, "endereco": endereco, "oferece_lt": oferece_lt}
+
+    def mostra_voluntario(self, dados_voluntario):
+        print("NOME DO VOLUNTARIO: ", dados_voluntario["nome"])
+        print("IDADE DO VOLUNTARIO: ", dados_voluntario["idade"])
+        print("TELEFONE DO VOLUNTARIO: ", dados_voluntario["telefone"])
+        print("GENERO DO VOLUNTARIO: ", dados_voluntario["genero"])
+        print("E-MAIL DO VOLUNTARIO: ", dados_voluntario["email"])
+        print("ENDERECO DO VOLUNTARIO: ", dados_voluntario["endereco"])
+        print("OFERECE LAR TEMPORARIO: ", dados_voluntario["oferece_lt"])
+        print("\n")
+
+    def seleciona_voluntario(self):
+        email = input("E-mail do voluntario que deseja selecionar: ")
+        return email
+
+    def mostra_mensagem(self, msg):
+        print(msg)
