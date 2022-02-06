fefc6e62a3ad87ab94c3379d76d1965b41dc0cbb
Carolina Pacheco <90946121+carolinapsil@users.noreply.github.com>
2022-02-06 13:03:59 -0300

    Ajustes classe Doacao

--- MVC/controle/controlador_doacao.py
+++ MVC/controle/controlador_doacao.py
@@ -24,7 +24,7 @@ class ControladorDoacoes():
         doacao = self.pega_doacao_por_codigo(codigo_doacao)
 
         if(doacao is not None):
-          novos_dados_doacao = self.__doacoes.pega_dados_doacao()
+          novos_dados_doacao = self.__tela_doacao.pega_dados_doacao()
           doacao.doador = novos_dados_doacao["doador"]
           doacao.valor = novos_dados_doacao["valor"]
           doacao.data_doacao = novos_dados_doacao["data_doacao"]
