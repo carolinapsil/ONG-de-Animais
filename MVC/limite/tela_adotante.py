299fb614ea619c1ad3cdf165824b5ec770ffeafc
Carolina Pacheco <90946121+carolinapsil@users.noreply.github.com>
2022-02-06 02:08:32 -0300

    Ajustes Tela Adotante

--- MVC/limite/tela_adotante.py
+++ MVC/limite/tela_adotante.py
@@ -1,11 +1,17 @@
+from MVC.entidade.adotante import Adotante
+from MVC.controle.controlador_adotante import ControladorAdotante
+
 class TelaAdotante:
+
+    def __init__(self, controlador_adotante):
+        self.__controlador_adotante = controlador_adotante
+
     def tela_opcoes(self):
         print("-------- ADOTANTE ----------")
         print("Escolha a opcao")
         print("1 - Incluir Adotante")
         print("2 - Alterar Adotante")
         print("3 - Listar Adotante")
-        print("4 - Excluir Adotante")
         print("0 - Retornar")
 
         opcao = int(input("Escolha a opcao: "))
@@ -20,7 +26,8 @@ class TelaAdotante:
         email = input("E-mail: ")
         endereco = input("Endereco: ")
 
-        return {"nome": nome, "data nascimento": data_nascimento, "telefone": telefone, "genero": genero, "email": email, "endereco": endereco}
+        return {"nome": nome, "data nascimento": data_nascimento, "telefone": telefone,
+                "genero": genero, "email": email, "endereco": endereco}
 
     def mostra_adotante(self, dados_adotante):
         print("NOME DO ADOTANTE: ", dados_adotante["nome"])
@@ -30,6 +37,12 @@ class TelaAdotante:
         print("E-MAIL DO ADOTANTE: ", dados_adotante["nome"])
         print("ENDERECO DO ADOTANTE: ", dados_adotante["nome"])
         print("\n")
+
+    def lista_adotantes(self):
+        for adotante in self.__adotantes:
+            self.__tela_adotante.mostra_adotante({"nome": adotante.nome, "data_nascimento": adotante.data_nascimento,
+                                                  "telefone": adotante.telefone, "cpf": adotante.cpf})
+
 
     def seleciona_adotante(self):
         email = input("E-mail do adotante que deseja selecionar: ")
