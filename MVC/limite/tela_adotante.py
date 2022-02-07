5b196ae96328da6627601506d7a4ebb21f7e0d07
Carolina Pacheco <90946121+carolinapsil@users.noreply.github.com>
2022-02-07 04:30:53 -0300

    Tratamento de exceções Tela Adotante

--- MVC/limite/tela_adotante.py
+++ MVC/limite/tela_adotante.py
@@ -29,16 +29,65 @@ class TelaAdotante:
 
     def pega_dados_adotante(self):
         print("-------- DADOS ADOTANTE ----------")
-        nome = input("Nome: ")
-        data_nascimento = input("data_nascimento: ")
-        telefone = input("Telefone: ")
-        genero = input("Genero: ")
-        email = input("E-mail: ")
-        endereco = input("Endereco: ")
-        idade = int(input("Idade: "))
+        while True:
+            try:
+                nome = input("Nome: ")
+                if nome == "":
+                    raise ValueError
+                break
+            except ValueError:
+                print("Por favor, não deixe o campo vazio!")
+        while True:
+            try:
+                data_nascimento = input("Data de nascimento: ")
+                if data_nascimento == "":
+                    raise ValueError
+                break
+            except ValueError:
+                print("Por favor, não deixe o campo vazio!")
+        while True:
+            try:
+                telefone = input("Telefone: ")
+                if telefone == "":
+                    raise ValueError
+                break
+            except ValueError:
+                print("Por favor, não deixe o campo vazio!")
+        while True:
+            try:
+                genero = input("Genero: ")
+                if (genero == "") or (genero.lower() != "feminino" and genero.lower() != "masculino"):
+                    raise ValueError
+                break
+            except ValueError:
+                print("Por favor, preencha o campo corretamente.")
+        while True:
+            try:
+                email = input("E-mail: ")
+                if (email == "") or ('@' not in email) or '.' not in email:
+                    raise ValueError
+                break
+            except ValueError:
+                print("E-mail invalido! Por favor, preencha o campo corretamente.")
+        while True:
+            try:
+                endereco = input("Endereco: ")
+                if endereco == "":
+                    raise ValueError
+                break
+            except ValueError:
+                print("Por favor, não deixe o campo vazio!")
+        while True:
+            try:
+                idade = (input("Idade: "))
+                if (idade.isnumeric() == False) or idade == "":
+                    raise ValueError
+                break
+            except ValueError:
+                print("Por favor, preencha o campo corretamente")
 
         return {"nome": nome, "data_nascimento": data_nascimento, "telefone": telefone,
-                "genero": genero, "email": email, "endereco": endereco, "idade": idade}
+                "genero": genero, "email": email, "endereco": endereco, "idade": int(idade)}
 
     def mostra_adotante(self, dados_adotante):
         print("NOME DO ADOTANTE: ", dados_adotante["nome"])
