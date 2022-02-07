0b02f3ef33f023a6c36bb52f84d3a6aa18de4392
Aline Cristina Meyer <aline.meyer@gmail.com>
2022-02-07 01:21:44 -0300

    remover classe cachorro e gato

--- MVC/entidade/cachorro.py
+++
@@ -1,8 +0,0 @@
-from MVC.entidade.animal import Animal
-
-
-class Cachorro(Animal):
-
-    def __init__(self, nome: str, chegada: str, ano_nascimento: str, sexo: str, doenca: str, vacina: str, castracao: str, porte: str):
-        self.__porte = porte
-        super.__init__(nome, chegada, ano_nascimento, sexo, doenca, vacina, castracao)
--- MVC/entidade/gato.py
+++
@@ -1,7 +0,0 @@
-from MVC.entidade.animal import Animal
-
-
-class Gato(Animal):
-
-    def __init__(self, nome: str, chegada: str, ano_nascimento: str, sexo: str, doenca: str, vacina: str, castracao: str):
-        super.__init__(nome, chegada, ano_nascimento, sexo, doenca, vacina, castracao)
