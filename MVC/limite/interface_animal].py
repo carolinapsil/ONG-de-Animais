626eb19c169c1d687b1ecf55a295d39ab7b683bc
Aline Cristina Meyer <aline.meyer@gmail.com>
2022-03-13 02:32:56 -0300

    Conserto Interface Animal

--- MVC/limite/interface_animal].py
+++ MVC/limite/interface_animal].py
@@ -10,29 +10,32 @@ class AnimalView:
     def init_components(self):
         sg.ChangeLookAndFeel("Reddit")
 
-    layout = [[sg.Text("Cadastre as informações do Animal!", size = (30, 1), font = ("Helvetica", 25))]
-              [sg.Text("Inserir o nome do Animal:")],
-              [sg.InputText("", key = "nome_animal")],
-              [sg.Text("Inserir a data de chegada:")],
-              [sg.InputText("", key = "data_chegada")],
-              [sg.Text("Inserir o ano de nascimento:")],
-              [sg.InputText("", key = "ano_nascimento")],
-              [sg.Text("Qual o sexo do Animal:")],
-              [sg.Radio("Fêmea", "RD1", default = True, key = "rd_femea"),
-               sg.Radio("Macho", "RD1", key = "rd_macho")],
-              [sg.Text("O animal possui doenças?:")],
-              [sg.Radio("Possui", "RD1", default = True, key = "rd_sim"),
-               sg.Radio("Não possui", "RD1", key = "rd_nao")],
-              [sg.Text("O Animal está vacinado?:")],
-              [sg.Radio("Sim", "RD1", default = True, key = "rd_vacinado"),
-               sg.Radio("Não", "RD1", key = "rd_naovacinado")],
-              [sg.Text("O Animal está castrado?:")],
-              [sg.Radio("Sim", "RD1", default = True, key = "rd_castrado"),
-               sg.Radio("Não", "RD1", key = "rd_naocastrado")],
-              [sg.Submit(), sg.Cancel()]
-             ]
+    column1 = [[sg.Text('Column 1', background_color='#F7F3EC',
+                justification='center', size=(10, 1))],
+                [sg.Spin(values=('Spin Box 1', '2', '3'), initial_value='Spin Box1')],
+                [sg.Spin(values=('Spin Box 1', '2', '3'), initial_value='Spin Box2')],
+                [sg.Spin(values=('Spin Box 1', '2', '3'), initial_value='Spin Box3')]]
 
-    self.__window = sg.Window("Cadastro de Animais", default_element_size == (40, 1).Layout(layout))
+    layout = [[sg.Text('Inserir os dados do Animal abaixo: ', size=(30, 1),
+                        justification='center', font=("Helvetica", 25), relief=sg.RELIEF_RIDGE)],
+                [sg.Text('Nome', size=(15, 1)), sg.InputText('nome', key='nome')],
+                [sg.Text('Chegada', size=(15, 1)), sg.InputText('chegada', key='chegada')],
+                [sg.Text('Ano Nascimento', size=(15, 1)), sg.InputText('ano_nascimento', key='ano_nascimento')],
+                [sg.Text("Qual o sexo do Animal:")],
+                [sg.Radio('Fêmea', "RADIO1", default=True, size=(10, 1)), sg.Radio('Macho', "RADIO1")],
+                [sg.Text("O Animal possui doenças?:")],
+                [sg.Radio('Sim', "RADIO1", default=True, size=(10, 1)), sg.Radio('Não', "RADIO1")],
+                [sg.Text("O Animal está vacinado?:")],
+                [sg.Radio('Sim', "RADIO1", default=True, size=(10, 1)), sg.Radio('Não', "RADIO1")],
+                [sg.Text("O Animal está castrado?:")],
+                [sg.Radio('Sim', "RADIO1", default=True, size=(10, 1)), sg.Radio('Não', "RADIO1")],
+                [sg.Submit(tooltip='Click to submit this window'), sg.Cancel()]]
 
+    window = sg.Window('Everything bagel', default_element_size=(40, 1), grab_anywhere = False).Layout(layout)
+    button, values = window.Read()
+    sg.Popup('Title',
+             'The results of the window.',
+             'The button clicked was "{}"'.format(button),
+             'The values are', values)
 
 
